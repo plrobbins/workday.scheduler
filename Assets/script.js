092ffbc380e7d2e74ca6aa04fb0bc3e59b0c17d3
Philip Robbins <plrobbins68@outlook.com>
2020-08-17 08:50:39 -0400

    updating the Javascript. Changing the color is not working

--- Assets/script.js
+++ Assets/script.js
@@ -3,8 +3,17 @@ var saveBtn = $(".saveBtn");
 var currentHour = moment().format("HH");
 var currentHourInt = parseInt(currentHour);
 
+
 //Data attributes for each hour
 $("9AMRow").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
+$("10AMRow").attr("data-time", moment("10:00 am", "h:mm a").format("HH"));
+$("11AMRow").attr("data-time", moment("11:00 am", "h:mm a").format("HH"));
+$("12PMRow").attr("data-time", moment("12:00 pm", "h:mm a").format("HH"));
+$("1PMRow").attr("data-time", moment("1:00 pm", "h:mm a").format("HH"));
+$("2PMRow").attr("data-time", moment("2:00 pm", "h:mm a").format("HH"));
+$("3PMRow").attr("data-time", moment("3:00 pm", "h:mm a").format("HH"));
+$("4PMRow").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
+$("5PMRow").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
 
 
 //Starting jQuery
